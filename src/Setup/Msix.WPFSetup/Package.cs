88907ce0e9946b2bff51934d173c172cd69836bd
俎羽峰 <3014085426@qq.com>
2025-06-25 10:35:24 +0800

    1.2.7 封包

--- src/Setup/Msix.WPFSetup/Package.cs
+++ src/Setup/Msix.WPFSetup/Package.cs
@@ -4,5 +4,5 @@ public static class Package
 {
     public static string PackageName => "鸣潮启动器";
 
-    public static string PackageVersion => "1.2.6";
+    public static string PackageVersion => "1.2.7";
 }
